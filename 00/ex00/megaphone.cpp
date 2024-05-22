8e3f7750f5799152b534bddf023ec8e48f81aecf
shatilovdr <shatilovdr@gmail.com>
2024-05-22 08:22:31 +0300

    start Module_00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 20:55:43 by dshatilo          #+#    #+#             */
+/*   Updated: 2024/05/21 21:29:46 by dshatilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+	(void)argc;
+	std::cout << argv[1];
+	return 0;
+}
