4352e85134fc0a992499555c1450511407c664ac
shatilovdr <shatilovdr@gmail.com>
2024-08-01 21:25:09 +0300

    mod05ex01: init commit

---
+++ 05/ex01/srcs/Bureaucrat.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 13:22:40 by dshatilo          #+#    #+#             */
+/*   Updated: 2024/08/01 21:09:46 by dshatilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+    : name_(name), grade_(grade) {
+  if (grade_ < 1) throw Bureaucrat::GradeTooHighException();
+  if (grade_ > 150) throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : name_(other.name_), grade_(other.grade_) {}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+  if (&other == this) {
+    return *this;
+  }
+  grade_ = other.grade_;
+  return *this;
+}
+
+const std::string& Bureaucrat::getName() const {
+  return name_;
+}
+
+const int& Bureaucrat::getGrade() const {
+  return grade_;
+}
+
+void Bureaucrat::promote() {
+  if (grade_ == 1) {
+    throw Bureaucrat::GradeTooHighException();
+  }
+  grade_ -= 1;
+}
+
+void Bureaucrat::demote() {
+  if (grade_ == 150) {
+    throw Bureaucrat::GradeTooLowException();
+  }
+  grade_ += 1;
+}
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& obj) {
+  out << obj.getName() << ", bureaucrat grade " << obj.getGrade() << '\n';
+  return out;
+}
---
+++ 05/ex01/srcs/main.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 21:02:53 by dshatilo          #+#    #+#             */
+/*   Updated: 2024/08/01 21:19:00 by dshatilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+void test0();
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+
+int main() {
+  test0();
+  test1();
+  test2();
+  test3();
+  test4();
+  // test5();
+}
+
+void test0() {
+  try {
+    Bureaucrat b("Alex", 0);
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+}
+
+void test1() {
+  try {
+    Bureaucrat b("Mikhail", 151);
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+}
+
+void test2() {
+  try {
+    Bureaucrat b("Bob", 3);
+    b.promote();
+    std::cout << b;
+    b.promote();
+    std::cout << b;
+    b.promote();
+    std::cout << b;
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+}
+
+void test3() {
+  try {
+    Bureaucrat b("Max", 149);
+    b.demote();
+    std::cout << b;
+    b.demote();
+    std::cout << b;
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+}
+
+void test4() {
+  try {
+    const Bureaucrat b("Robert", 149);
+    std::cout << b;
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+}
+
+void test5() {
+    Bureaucrat b("Sam", 1);
+    std::cout << b;
+    b.promote();
+}
