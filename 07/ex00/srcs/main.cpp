c1b1d8ca28331a5fdf277a791794f6f994ab5b2c
Dmitrii Shatilov <shatilovdr@gmail.com>
2024-08-08 18:22:43 +0300

    mod07ex00: init

---
+++ 07/ex00/srcs/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 18:03:03 by dshatilo          #+#    #+#             */
+/*   Updated: 2024/08/08 18:19:04 by dshatilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Whatever.hpp"
+
+int main(void) {
+  int a = 2;
+  int b = 3;
+
+  ::swap( a, b );
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+  return 0;
+}
