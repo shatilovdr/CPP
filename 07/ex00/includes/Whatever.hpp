58433d5c08deeb774a0c11f4932f697105a28e41
Dmitrii Shatilov <shatilovdr@gmail.com>
2024-08-09 11:50:32 +0300

    mod07ex00: finish exercise

--- 07/ex00/includes/Whatever.hpp
+++ 07/ex00/includes/Whatever.hpp
@@ -6,11 +6,28 @@
 /*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 18:03:18 by dshatilo          #+#    #+#             */
-/*   Updated: 2024/08/08 18:07:00 by dshatilo         ###   ########.fr       */
+/*   Updated: 2024/08/09 11:47:15 by dshatilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP_
 #define WHATEVER_HPP_
+
+template <typename T> 
+void swap(T& a, T&b) {
+  T c = a;
+  a = b;
+  b = c;
+}
+
+template <typename T>
+T& min(T& a, T&b) {
+  return a < b ? a : b;
+}
+
+template <typename T>
+T& max(T& a, T&b) {
+  return a > b ? a : b;
+}
 
 #endif
