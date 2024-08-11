49c24b04a39bd3d7af4662b4eea95fa726bcd5a0
Dmitrii Shatilov <shatilovdr@gmail.com>
2024-08-11 18:51:41 +0300

    mod08ex01: add Span declaration

--- 08/ex01/includes/Span.hpp
+++ 08/ex01/includes/Span.hpp
@@ -6,11 +6,32 @@
 /*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 17:42:04 by dshatilo          #+#    #+#             */
-/*   Updated: 2024/08/11 17:45:26 by dshatilo         ###   ########.fr       */
+/*   Updated: 2024/08/11 18:43:56 by dshatilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP_
 #define SPAN_HPP_
+
+#include <exception>
+#include <vector>
+
+
+class Span {
+ public:
+  Span() = delete;
+  Span(unsigned int cap);
+  Span(const Span& other) = default;
+  Span& operator=(const Span& other) & = default;
+
+  ~Span() = default;
+
+  unsigned int shortestSpan() const;
+  unsigned int longestSpan() const;
+  void         addNumber(int number);
+  void         addNumber(int number, unsigned int);
+
+};
+
 
 #endif
