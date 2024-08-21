7010d0219551bfdb900eae6133de20c8ed832ed9
Dmitrii Shatilov <shatilovdr@gmail.com>
2024-08-21 11:59:28 +0300

    mod09ex01: init

---
+++ 09/ex01/includes/RPN.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 11:55:17 by dshatilo          #+#    #+#             */
+/*   Updated: 2024/08/21 11:58:42 by dshatilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP_
+#define RPN_HPP_
+
+class BitcoinExchange {
+ public:
+  BitcoinExchange()                                        = delete;
+  BitcoinExchange(const BitcoinExchange& other)            = delete;
+  BitcoinExchange& operator=(const BitcoinExchange& other) = delete;
+
+  ~BitcoinExchange() = delete;
+
+};
+
+#endif
